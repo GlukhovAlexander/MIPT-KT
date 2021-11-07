382bb4ca1d005480049b10e6c9a46c8dd2d86390
Alexander Glukhov <93672169+mezberg@users.noreply.github.com>
2021-11-07 17:27:11 +0300

    Update 2_2_using_dprintf.c

--- 2_2_using_dprintf.c
+++ 2_2_using_dprintf.c
@@ -11,27 +11,34 @@
 
 int main(int argc, char * argv[]) 
 {
+//Выход с ошибкой, если неправильный ввод
 	if (argc != 3) 
 	{
 		fprintf(stderr, "Usage: %s path text", argv[0]); 
 		return 1; 
 	}
-	
-	int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); 
 
+//Создание переменной fd
+	int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); //Если файл не открылся для записи, fd = -1; если файла нет, то создаём, если в файле есть содержимое, стираем его
+
+
+
+//Выход с ошибкой, если файл не окрылся для чтения
 	if (fd < 0) 
 	{
-		perror("Failed to open file for writing"); 
+		perror("File cannot be opened for writing"); //вывод в поток ошибок
 		return 2;
 	}
 
 	if (dprintf(fd,"%s", argv[2]) < 0)
 	{
-		perror("Failed to write"); 
+		perror("Failure during writing"); 
 		close(fd); 
 		return 3;
 	}
 
+	
+//Выход с ошибкой, если файл не закрылся
 	if (close(fd) < 0) 
 	{
 		perror("Failure during close"); 
