0ae0fb2d23e14257f337c156e1c7e73eb0add853
alexander <you@example.com>
2021-12-13 22:15:37 +0300

    1 fixed

--- 1_stat.c
+++ 1_stat.c
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/sysmacros.h>
 #include <string.h>
 #include <linux/stat.h>
 #include <linux/fcntl.h>
@@ -12,12 +11,11 @@
 #include <stdint.h>
 #include <grp.h>
 #include <pwd.h>
-#define _GNU_SOURCE
 
 
 // Функция для вывода даты и времени с миллисекундами	
 
-static void print_time(const char *type, struct timespec *ts) {
+static void print_time(const char *type, const struct timespec *ts) {
 
         time_t tv_sec = (time_t) ts->tv_sec;
         char buffer[sizeof("YYYY-mm-dd HH:MM:SS")];
@@ -50,7 +48,7 @@ char * rights (mode_t mode)
 	char * mode_rights = malloc (sizeof("rwxrwxrwx"));
 	int i = 8;
 	for (i = 8; i >= 0; i--)
-	       mode_rights[8-i] = (mode & (1 << i) ? "xwr"[i%3]: '-');
+	       mode_rights[8-i] = (mode & (1u << i) ? "xwr"[i%3]: '-');
 	mode_rights[9] = '\0';
 	return mode_rights;
 }
@@ -82,13 +80,12 @@ int main(int argc, char *argv[])
 	printf("File name %s\n", argv[1]);
 	printf("ID of containing device: [%lxh, %ldd]\n", (long) (sb.st_dev), (long) (sb.st_dev));
 	printf("File type: %s\n", filetype(sb.st_mode));
-	printf("%s", filetype(sb.st_mode));
 	printf("I-node number: %ld\n", (uintmax_t) sb.st_ino);
 	char *permissions_str = rights(sb.st_mode);
 	printf("Mode_rights: (%04o/%s)\n", sb.st_mode & ALLPERMS, permissions_str);
 	free(permissions_str);
 	printf("Link count: %ld\n", (long) sb.st_nlink);
-	printf("Ownership UID=%ld/\t%s   GID=%ld/\t%s\n", (long) sb.st_uid, user_name(sb.st_uid), (long) sb.st_gid, group_name(sb.st_gid));
+	printf("Ownership UID=%ld/%s   GID=%ld/%s\n", (long) sb.st_uid, user_name(sb.st_uid), (long) sb.st_gid, group_name(sb.st_gid));
 	printf("Preferred I/O block size: %ld bytes\n", (long) sb.st_blksize);
 	printf("File size: %lld bytes\n", (long long) sb.st_size);
 	printf("Blocks allocated: %lld\n", (long long) sb.st_blocks);
