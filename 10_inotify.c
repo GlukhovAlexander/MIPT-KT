4b5933eb51ea7abcd093de05910c3260185ec313
alexander <you@example.com>
2021-12-18 22:03:14 +0300

    10 tabs added

--- 10_inotify.c
+++ 10_inotify.c
@@ -8,105 +8,106 @@
 
 #define SIZEOF_BUF  4096
 //проходим по всем событиям, которые можем прочитать из файлового дескриптора 
-void handle_event(int fd, int wd, char* pathname) { 
-    char buf[SIZEOF_BUF];
-    const struct inotify_event *event;
-    ssize_t len;
-    char *ptr;
-    for(;;) {
-        len = read(fd, buf, sizeof(buf));
-        if(len == -1 && errno != EAGAIN) {
-            perror("read");
-            exit(EXIT_FAILURE);
-        }
+void handle_event(int fd, int wd, char* pathname) { 	
+	char buf[SIZEOF_BUF];
+	const struct inotify_event *event;
+	ssize_t len;
+	char *ptr;
+	for(;;) {
+        	len = read(fd, buf, sizeof(buf));
+        	if(len == -1 && errno != EAGAIN) {
+            	perror("read");
+            	exit(EXIT_FAILURE);
+	}
 //Если неблокирующий read() не найдёт событий для чтения, то вернёт -1 с errno равным EAGAIN, тогда выходим из цикла
-        if(len <= 0) {
-            break;
+	if(len <= 0) {
+	       	break;
         }
 //проходим по всем событиям в буфере
-        for(ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
-            event = (const struct inotify_event*)buf;
+	for(ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len) {
+	event = (const struct inotify_event*)buf;
 // печатаем тип события
-            if(event->mask & IN_CREATE) {
-                printf("IN_CREATE: ");
-            }
+	if(event->mask & IN_CREATE) {
+		printf("IN_CREATE: ");
+	}
 
-	    if((event->cookie != 0) && (event -> mask & IN_MOVE)) {
+	if((event->cookie != 0) && (event -> mask & IN_MOVE)) {
 		printf("IN_MOVE (rename): ");
         }
 
         if(wd == event->wd) {
-            printf("%s/", pathname);
+		printf("%s/", pathname);
         }
         if(event->len) {
-            printf("%s", event->name);
+        	printf("%s", event->name);
         }
 
         if(event->mask & IN_ISDIR) {
-            printf(" [directory]\n");
-        } else {
-            printf(" [file]\n");
-        }
-    }
+        	printf(" [directory]\n");
+        } 
+	else 	{
+            	printf(" [file]\n");
+        	}
+    	}
 }
 }
 
 
 int main(int argc, char* argv[]) {
-    if(argc != 2) {
-        perror("Need 2 arguments");
-        exit(EXIT_FAILURE);
-    }
+    	if(argc != 2) {
+        	perror("Need 2 arguments");
+        	exit(EXIT_FAILURE);
+    	}
 
-    char buf;
-    int fd, poll_num, wd;
-    nfds_t nfds;
-    struct pollfd fds[2];
+    	char buf;
+    	int fd, poll_num, wd;
+    	nfds_t nfds;
+    	struct pollfd fds[2];
 // Создаём файловый дескриптор для доступа к inotify API
-    fd = inotify_init1(IN_NONBLOCK);
-    if(fd == -1) {
-        perror("inotify_init1");
-        exit(EXIT_FAILURE);
-    }
+    	fd = inotify_init1(IN_NONBLOCK);
+    	if(fd == -1) {
+        	perror("inotify_init1");
+        	exit(EXIT_FAILURE);
+    	}
 // выделяем память под дескрипторы 
-    wd = inotify_add_watch(fd, argv[1], IN_CREATE| IN_MOVE);
-    if(wd == -1) {
-        perror("inotify_add_watch");
-        exit(EXIT_FAILURE);
-    }
+    	wd = inotify_add_watch(fd, argv[1], IN_CREATE| IN_MOVE);
+    	if(wd == -1) {
+        	perror("inotify_add_watch");
+        	exit(EXIT_FAILURE);
+    	}
 
-    nfds = 2;
-    fds[0].fd = STDIN_FILENO;
-    fds[0].events = POLLIN;
-    fds[1].fd = fd;
-    fds[1].events = POLLIN;
-    printf("Listening for events.\n");
+    	nfds = 2;
+    	fds[0].fd = STDIN_FILENO;
+    	fds[0].events = POLLIN;
+    	fds[1].fd = fd;
+    	fds[1].events = POLLIN;
+    	printf("Listening for events.\n");
 //ждём события и/или ввода с терминала 
-    while(1) {
-        poll_num = poll(fds, nfds, -1);
+    	while(1) {
+        	poll_num = poll(fds, nfds, -1);
         if(poll_num == -1) {
-            if (errno == EINTR) {
-                continue;
-            }
-            perror("poll");
-            exit(EXIT_FAILURE);
+            	if (errno == EINTR) {
+                	continue;
+           	 }	
+            	perror("poll");
+            	exit(EXIT_FAILURE);
         }
         if(poll_num > 0) {
-            if (fds[0].revents & POLLIN) {
+      	if (fds[0].revents & POLLIN) {
 //опустошаем stdin и выходим  
-      		    while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
-                    continue;
-                break;
-            }
-	    if (fds[1].revents & POLLIN) {
+     	while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
+        	continue;
+   	break;
+   	}
+       	if (fds[1].revents & POLLIN) {
 //доступны события
-    		    handle_event(fd, wd, argv[1]);
-            }
+	handle_event(fd, wd, argv[1]);
+	}
         
 	}
-    }
+ 	}
 
-    printf("Listening for events stopped.\n");
-    close(fd);
-    return 0;
+    	printf("Listening for events stopped.\n");
+    	close(fd);
+    	return 0;
 }
