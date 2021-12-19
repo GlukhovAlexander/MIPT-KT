bf912484b65c3180771b01b7a858c16e9f530c58
alexander <you@example.com>
2021-12-19 19:09:14 +0300

    getgrouplist -> getgroups, tabs fixed

--- 12_process_inf.c
+++ 12_process_inf.c
@@ -1,54 +1,112 @@
 //вывести информацию про текущий процесс (про запущенный экземпляр этой программы)
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <string.h>
 #include <sys/types.h>
+#include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
-int main(void)
-{
-//Выводим id процесса, родительского процесса и сессии.
-    printf ("Process ID: %d, Parent Pricess ID: %d, Session ID: %d\n", getpid(), getppid(), getsid(0));
-//Чтобы вывести не только User Id(находим с помощью getuid), но и User Name используем файл passwd, где по id  вытаскиваем имя
-    struct passwd *pswd = getpwuid(getuid());
-    if (pswd == NULL)
-    {
-        perror("Error in reading /etc/passwd"); //выход с ошибкой
 
-        return 1;
-    }
-    printf("User ID: %d, User Name: %s\n", getuid(), pswd->pw_name);
-//Аналогично для Group ID и Group Name, только файл уже group
-    struct group *grp = getgrgid(pswd->pw_gid);
-    if (grp == NULL)
-    {
-        perror("Error in reading /etc/group"); //выход с ошибкой
+int main(void) {
 
-        return 2;
-    }
-    printf("Group ID: %d, Group Name: %s\n", getgid(), grp->gr_name);
 
-//Выводим не только активную группу - в которой user, но и остальные
-    long ngroups_max = sysconf(_SC_NGROUPS_MAX);
-    gid_t *groups = (gid_t *)malloc(ngroups_max *sizeof(gid_t));
-    int ngroups = getgroups(ngroups_max, groups);
-    if (ngroups == -1)
-    {
-        perror("Error in getting groups"); //выход с ошибкой
-        return 3;
-    }
-    printf("Groups: ");
-    struct group *supgrp = getgrgid(pswd->pw_gid);
-    int i = 0;
-    for (i = 0; i < ngroups - 1; i++)
-    {
-        printf("%d ", groups[i]);
-        supgrp = getgrgid(groups[i]);
-        printf("(%s); ", supgrp->gr_name);
-    }
-    printf("\n");
 
-    return 0;
+	// PID, PPID
+	// Each process has a unique non-negative integer identifier that is assigned when the process is created using fork -- PID
+	// A process's parent process ID identifies the process thet created this process using fork(2) -- PPID
+	pid_t process_id = getpid();
+	pid_t parent_pid = getppid();
+	printf("Process ID: %d\nParent's process ID: %d\n", process_id, parent_pid);
+	//Process Group ID and Session ID
+	pid_t process_group_id = getpgrp();
+	pid_t session_id = getsid(process_id);
+	printf("Process Group ID: %d\nSession ID: %d\n", process_group_id, session_id);
+
+	//Имя пользователя
+	uid_t uid = getuid();
+	struct passwd* pass_info;
+	const char* user_name;
+	pass_info = getpwuid(uid);
+	if(pass_info == NULL) {
+        	user_name = "?";
+	} else {
+	user_name = pass_info->pw_name;
+	}
+	printf("User ID = %d, %s\n", uid, user_name);
+
+
+	//Идентификаторы user and group
+	//Идентификаторы владельцев процесса (пользователя и группы)
+
+	gid_t gid = getgid();
+	const char* grp_name;
+	struct group* grp_info = getgrgid(gid);
+	if(grp_info == NULL) {
+		grp_name = "?";
+	} else {
+		grp_name = grp_info->gr_name;
+	}
+	printf("Group ID = %d, %s\n", gid, grp_name);
+
+	//Effective user ID и effective group ID
+
+	printf("Effective user ID = %d\n", geteuid());
+	printf("Effective group ID = %d\n", getegid());
+
+
+	//Все группы
+	int j, ngroups;
+	gid_t *groups;
+	struct passwd *pw;
+	struct group *gr;
+	/если размер size равен нулю, и список равен NULL, то просто возвращается
+	//общее количество идентификаторов дополнительных групп для текущего процесса
+	ngroups = getgroups(0, NULL);
+	groups = (gid_t *)calloc((size_t)ngroups, sizeof (gid_t));
+	if (groups == NULL) {
+		perror("calloc");
+		exit(EXIT_FAILURE);
+	}
+	//Указатель на структуру с паролями
+	//pw = getpwnam(user_name);
+	if (pw == NULL) {
+		perror("getpwnam");
+		exit(EXIT_SUCCESS);
+	}
+	//Группа к которой принадлежит пользователь
+	if (getgroups(ngroups, groups) == -1) {
+		fprintf(stderr, "getgrouplist() вернула -1; ngroups = %d\n",
+		ngroups);
+	exit(EXIT_FAILURE);
+	}
+	//Выводим список групп с именами
+	for (j = 0; j < ngroups; j++) {
+		printf("Group (name): %d", groups[j]);
+		gr = getgrgid(groups[j]);
+	if (gr != NULL) {
+		printf(" (%s)", gr->gr_name);
+	}
+	printf("\n");
+	}
+
+    
+	mode_t u_mask = umask(ALLPERMS);
+	printf("Umask: %o\n", u_mask);
+	//umask устанавливает значение umask равным mask & 0777 (используются только биты прав доступа к файлам маски)
+	//меняет текущее значение маски, поэтому нужно вернуть предыдущее значение
+	u_mask = umask(u_mask);
+
+	//получаем приоритет планирования
+	int pr = getpriority(PRIO_PROCESS, 0);
+	if(pr == -1) {
+		perror("Failure getpriority");
+		exit(EXIT_FAILURE);
+	}
+	printf("Sheduling priority: %d\n", pr);
+	return 0;
 }
